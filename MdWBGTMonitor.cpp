a1bd1f11542b314249bcb6a318e17b55a89505df
kumi_sys07 <kumikomi20230407@gmail.com>
2023-06-15 15:10:55 +0900

    計算結果格納用変数の型及び変数名の変更

--- MdWBGTMonitor.cpp
+++ MdWBGTMonitor.cpp
@@ -10,15 +10,15 @@ void MdWBGTMonitor::init(){
 
 void MdWBGTMonitor::getWBGT(double* temperature, double* humidity, WbgtIndex* index){
     dthsen.getTempHumi(temperature, humidity);
-    double state = 0.68 * (*temperature) + 0.12 * (*humidity);
+    int calc_index = 0.68 * (*temperature) + 0.12 * (*humidity);
 
-    if(state <= 15){
+    if(calc_index <= 15){
         *index = DANGER;
-    } else if(state <= 24) {
+    } else if(calc_index <= 24) {
         *index = ATTENTION;
-    } else if(state <= 27) {
+    } else if(calc_index <= 27) {
         *index = ALERT;
-    } else if(state <=  30) {
+    } else if(calc_index <=  30) {
         *index = HIGH_ALERT;
     } else {
         *index = DANGER;
